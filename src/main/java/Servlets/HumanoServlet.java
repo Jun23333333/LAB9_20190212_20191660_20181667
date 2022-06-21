08d50ea83594ed05ef5dde949423f96721f25519
Jorge Guillen Rivera <jorgegrr052002@gmail.com>
2022-06-21 15:53:05 -0500

    corregido Humano beany dao

--- src/main/java/Servlets/HumanoServlet.java
+++ src/main/java/Servlets/HumanoServlet.java
@@ -1,11 +1,13 @@
 package Servlets;
 
+import Beans.HumanoBean;
 import Daos.HumanoDao;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @WebServlet(name = "HumanoServlet", value = "")
 public class HumanoServlet extends HttpServlet {
@@ -13,17 +15,18 @@ public class HumanoServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String action = request.getParameter("action") == null ? "listar" : request.getParameter("action");
         HumanoDao humanoDao = new HumanoDao();
+        RequestDispatcher view;
         switch (action){
             case "listar":
-                request.setAttribute("listaHumano",humanoDao.listaHumano());
-                RequestDispatcher requestDispatcher = request.getRequestDispatcher("Humano.jsp");
-                requestDispatcher.forward(request,response);
+                ArrayList<HumanoBean> lista = humanoDao.listarHumano();
+                request.setAttribute("listaHumano",lista);
+                System.out.println(lista);
+                view = request.getRequestDispatcher("Humano.jsp");
+
+                view.forward(request,response);
                 break;
         }
     }
 
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    }
 }
